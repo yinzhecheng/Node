c8a53a24ecfc45616b72d1df91fe3d4718f8f93a
yinzhecheng <1360600014@qq.com>
2017-11-06 01:07:59 +0800

    '234'

---
+++ http/webroot/demo2/src/components/counter.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<button @click="increment">+</button>
+		<button @click="decrement">-</button>
+		<p>{{num}}</p>
+	</div>
+</template>
+
+<script>
+	export default{
+		props:['num'],
+		data(){
+			return {
+				num:0,
+//				msg:'hello world!'
+			}
+		},
+		methods:{
+			increment(){
+				this.$emit('incre')
+			},
+			decrement(){
+				this.$emit('decre')
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
